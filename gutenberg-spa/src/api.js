f4ae38451354b99fd2e38285a02adacd57ec8d41
Leen Ashri <lsashri@ncsu.edu>
2025-05-14 18:12:33 +0300

    Fix: Use backend URL for API

--- gutenberg-spa/src/api.js
+++ gutenberg-spa/src/api.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://127.0.0.1:8000';
+const API_BASE_URL = "https://gutenberg-backend.onrender.com";
 
 export async function fetchAnalysis(bookId) {
   const res = await fetch(`${API_BASE}/book/${bookId}`);
